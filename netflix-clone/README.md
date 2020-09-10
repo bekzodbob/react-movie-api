da1cb22c7b50f895bb224704559a990c146736b3
bekzodbob <69902026+bekzodbob@users.noreply.github.com>
2020-09-10 20:05:21 +0500

    Add files via upload

---
+++ netflix-clone/README.md
@@ -0,0 +1,20 @@
+### Steps to run project
+* provide imdb API_KEY in requests.js 
+* npm install
+* npm start
+
+### My node and react learnings from this project 
+
+* how to make project component based and pass props
+* how to use react hooks : useState and useEffect
+*  about axios(Promise based HTTP client for the browser and node.js)
+*  about OR operator {movie?.title || movie?.name || movie?.original_name).It helps to avoid if else
+* got to know about node libraries
+      1. npm i movie-trailer
+      2. npm i react-youtube
+* learnt about concept of destructuring
+
+
+
+
+
---
+++ netflix-clone/build/precache-manifest.ad1d3dfd88ada1ad9354dddcb1906b22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7341c542ea64563e4f38585746d73dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f440628b6271b3482e21",
+    "url": "/static/css/main.5f29e605.chunk.css"
+  },
+  {
+    "revision": "4729b7c7a8e62029915c",
+    "url": "/static/js/2.3f487d65.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.3f487d65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f440628b6271b3482e21",
+    "url": "/static/js/main.0b04af8d.chunk.js"
+  },
+  {
+    "revision": "07b33697149324db54e5",
+    "url": "/static/js/runtime-main.b8a87127.js"
+  }
+]);
